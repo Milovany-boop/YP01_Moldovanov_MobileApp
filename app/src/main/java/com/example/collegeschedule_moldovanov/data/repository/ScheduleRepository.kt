1477fb766e52815d3e6c1950070b1c5e9e75df53
Milovany-boop <moldovanov.matveyka@gmail.com>
2026-03-17 00:32:38 +0700

    Настройка сетевого взаимодействия

---
+++ app/src/main/java/com/example/collegeschedule_moldovanov/data/repository/ScheduleRepository.kt
@@ -0,0 +1,10 @@
+package com.example.collegeschedule_moldovanov.data.repository
+
+import com.example.collegeschedule_moldovanov.data.api.ScheduleApi
+import com.example.collegeschedule_moldovanov.data.dto.ScheduleByDateDto
+
+class ScheduleRepository(private val api: ScheduleApi) {
+    suspend fun loadSchedule(group: String, start: String, end: String): List<ScheduleByDateDto> {
+        return api.getSchedule(group, start, end)
+    }
+}
